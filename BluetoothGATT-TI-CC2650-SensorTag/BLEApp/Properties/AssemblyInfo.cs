ffe5f3dc0a4856e13e9e838b729c66041f620368
djaus <davidjones@sportronics.com.au>
2017-02-25 23:37:37 +1100

    Didn't update Asset iamges for Store

--- BluetoothGATT-TI-CC2650-SensorTag/BLEApp/Properties/AssemblyInfo.cs
+++ BluetoothGATT-TI-CC2650-SensorTag/BLEApp/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BluetoothGATT-CC2650")]
-[assembly: AssemblyDescription("Sample app to demonstrate use of TI CC2650 BLE SensorTag")]
+[assembly: AssemblyTitle("CC2650SensorTag")]
+[assembly: AssemblyDescription("Sample UWP app to demonstrate use of TI CC2650 BLE SensorTag")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Sportronics")]
-[assembly: AssemblyProduct("BluetoothGATT")]
+[assembly: AssemblyProduct("BluetoothGATT CC2650SensorTag")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -24,6 +24,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("3.0.0.0")]
+[assembly: AssemblyFileVersion("3.0.0.0")]
 [assembly: ComVisible(false)]
