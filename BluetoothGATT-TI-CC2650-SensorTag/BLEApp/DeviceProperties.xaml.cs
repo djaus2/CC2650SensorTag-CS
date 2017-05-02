2cc559f8b9e6a12d005d0cf6feaf2bc37de12599
David Jones <davidjones@sportronics.com.au>
2017-05-02 21:57:18 +1000

    A bug fix with getting SysInfo. Needed async UI  Dispatcher for some code. May be an OS change in Build 16184

--- BluetoothGATT-TI-CC2650-SensorTag/BLEApp/DeviceProperties.xaml.cs
+++ BluetoothGATT-TI-CC2650-SensorTag/BLEApp/DeviceProperties.xaml.cs
@@ -14,6 +14,7 @@ using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using TICC2650SensorTag;
 using System.Threading.Tasks;
+using Windows.UI.Core;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -62,56 +63,58 @@ namespace BluetoothGATT
 
             int count = 0;
 
-
-            foreach (CC2650SensorTag.SensorTagProperties val in props.Keys)
+            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal,  () =>
             {
-                byte[] bytes = props[val];
-                string Valstr = "";
-                if (bytes != null)
+                foreach (CC2650SensorTag.SensorTagProperties val in props.Keys)
                 {
-
-                    count++;
-                    TextBlock PropBlock = (TextBlock)this.FindName("Name" + count.ToString());
-                    TextBlock ValueBlock = (TextBlock)this.FindName("Value" + count.ToString());
-                    if (!CC2650SensorTag.showbytes.Contains(val))
+                    byte[] bytes = props[val];
+                    string Valstr = "";
+                    if (bytes != null)
                     {
-                        Valstr = System.Text.Encoding.UTF8.GetString(bytes);
-                        if (Valstr != null)
-                            if (Valstr != "")
+
+                        count++;
+                        TextBlock PropBlock = (TextBlock)FindName("Name" + count.ToString());
+                        TextBlock ValueBlock = (TextBlock)this.FindName("Value" + count.ToString());
+                        if (!CC2650SensorTag.showbytes.Contains(val))
+                        {
+                            Valstr = System.Text.Encoding.UTF8.GetString(bytes);
+                            if (Valstr != null)
+                                if (Valstr != "")
+                                {
+                                    PropBlock.Text = val.ToString();
+                                    ValueBlock.Text = Valstr;
+                                }
+
+                        }
+                        else
+                        {
+                            if (bytes != null)
                             {
-                                PropBlock.Text = val.ToString();
+                                PropBlock.Text = val.ToString() + "[" + bytes.Length.ToString() + "]";
+
+                                for (int i = 0; i < bytes.Length; i++)
+                                {
+                                    Valstr += " " + bytes[i].ToString("X2");
+                                }
                                 ValueBlock.Text = Valstr;
                             }
-
-                    }
-                    else
-                    {
-                        if (bytes != null)
-                        {
-                            PropBlock.Text = val.ToString() + "[" + bytes.Length.ToString() + "]";
-
-                            for (int i = 0; i < bytes.Length; i++)
-                            {
-                                Valstr += " " + bytes[i].ToString("X2");
-                            }
-                            ValueBlock.Text = Valstr;
+                            //NB:
+                            //    Re: PNP_ID App got: pnp_id[7] { 01 0D 00 00 00 10 01 }
+                            //    From:
+                            //    https://e2e.ti.com/support/wireless_connectivity/bluetooth_low_energy/f/538/p/434053/1556237
+                            //
+                            //    In devinfoservice.c, you can find vendor ID and product ID information below where TI's vendor ID is 0x000D. 
+                            //    static uint8 devInfoPnpId[DEVINFO_PNP_ID_LEN] ={ 
+                            //    1, // Vendor ID source (1=Bluetooth SIG) 
+                            //    LO_UINT16(0x000D), HI_UINT16(0x000D), // Vendor ID (Texas Instruments) 
+                            //    LO_UINT16(0x0000), HI_UINT16(0x0000), // Product ID (vendor-specific) 
+                            //    LO_UINT16(0x0110), HI_UINT16(0x0110) // Product version (JJ.M.N)};  
+                            // 
                         }
-                        //NB:
-                        //    Re: PNP_ID App got: pnp_id[7] { 01 0D 00 00 00 10 01 }
-                        //    From:
-                        //    https://e2e.ti.com/support/wireless_connectivity/bluetooth_low_energy/f/538/p/434053/1556237
-                        //
-                        //    In devinfoservice.c, you can find vendor ID and product ID information below where TI's vendor ID is 0x000D. 
-                        //    static uint8 devInfoPnpId[DEVINFO_PNP_ID_LEN] ={ 
-                        //    1, // Vendor ID source (1=Bluetooth SIG) 
-                        //    LO_UINT16(0x000D), HI_UINT16(0x000D), // Vendor ID (Texas Instruments) 
-                        //    LO_UINT16(0x0000), HI_UINT16(0x0000), // Product ID (vendor-specific) 
-                        //    LO_UINT16(0x0110), HI_UINT16(0x0110) // Product version (JJ.M.N)};  
-                        // 
                     }
                 }
-            }
-       
+            });
+
         }//End of fn 
 
         private void Page_Loaded(object sender, RoutedEventArgs e)
